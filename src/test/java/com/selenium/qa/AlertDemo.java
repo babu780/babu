cf8aec0e56f5df5aa158ec76b926c16ecee379bd
unknown <nasrullah.it.qa@gmail.com>
2022-03-21 21:33:48 -0400

    new test

---
+++ src/test/java/com/selenium/qa/AlertDemo.java
@@ -0,0 +1,27 @@
+package com.selenium.qa;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import static org.bouncycastle.crypto.tls.ContentType.alert;
+
+public class AlertDemo {
+    @Test
+    public void alerTest() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.hyrtutorials.com/p/alertsdemo.html");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("alertBox")).click();
+        Thread.sleep(10000);
+        System.out.println("alerTest");cd
+
+
+
+
+    }
+}
